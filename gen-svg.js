22d665af830fcfd690590b27c7d301dbc37fd1e7
Owen Wu <yubing744@163.com>
2023-11-12 11:18:38 +0800

    feat: gen qzw svg

---
+++ gen-svg.js
@@ -0,0 +1,34 @@
+function utf8ToHex(str) {
+  let buffer = Buffer.from(str, 'utf8');
+  return buffer.toString('hex');
+}
+
+function genSvg(char, utf8Hex) {
+  return `<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+  <rect width="100%" height="100%" fill="#ffffff"/>
+  <text x="50%" y="45%" font-family="Arial" font-size="50" fill="black" dominant-baseline="middle" text-anchor="middle">${char}</text>
+  <text x="50%" y="75%" font-family="Arial" font-size="20" fill="black" dominant-baseline="middle" text-anchor="middle">${utf8Hex}</text>
+</svg>`
+}
+
+function createDirAndWrite(dir, filename, content) {
+  fs.mkdirSync(dir, { recursive: true });
+  fs.writeFileSync(`${dir}/${filename}`, content);
+}
+
+const fs = require('fs');
+
+fs.readFile('qzw.txt', 'utf8', function(err, data) {
+  if (err) throw err;
+
+  const characters = data.split('');
+  let index = 0;
+  characters.forEach((character) => {
+    if (character=='，' || character=='。' || character=='\n') {
+      return
+    }
+
+    const svgContent = genSvg(character, utf8ToHex(character))
+    createDirAndWrite("./dist", index++ + ".svg", svgContent)
+  });
+});
